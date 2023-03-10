73988aa9cc1a8efb93064bb012b4da8e96598aa2
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-03-10 14:47:43 +0200

    simplelightbox

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 import fetchImages from './fetch-images';
 import cardTemplate from '../template/card-template.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const { searchForm, gallery, loadMoreBtn, endCollectionText } = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  endCollectionText: document.querySelector('.end-collection-text'),
+};
