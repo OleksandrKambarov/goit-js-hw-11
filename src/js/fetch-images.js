81e5fe7581b77e23d6028a61597b404fe6a89097
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-03-11 23:35:14 +0200

    await-response-data

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -5,5 +5,6 @@ export default async function fetchImages(value, page) {
   const key = '34288902-13701de51ab67ccd3010f5c4f';
   const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
 
-  return await axios.get(`${url}${filter}`).then(response => response.data);
+  const response = await axios.get(`${url}${filter}`);
+  return await response.data;
 }
